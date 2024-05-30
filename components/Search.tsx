4714ab1a56559bfa15a02b1ab64f3aeb2a384b09
Akin Akinbobola <akinbobola71@gmail.com>
2024-05-30 10:26:33 +0100

    Fix dropdown

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,12 +3,20 @@
 import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 import { searchWeather } from "@/app/actions/weatherActions";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const router = useRouter();
   const [searchInput, setSearchInput] = useState("");
   const [debouncedSearchInput] = useDebounce(searchInput, 500);
   const [showDropdown, setShowDropdown] = useState(false);
   const [suggestions, setSuggestions] = useState<SearchResults[]>([]);
+
+  const handleSearch = (query: string) => {
+    setSearchInput(query);
+    setShowDropdown(false);
+    router.push(`/?search=${query}`);
+  };
 
   useEffect(() => {
     const fetchSuggestions = async () => {
@@ -16,14 +24,9 @@ const Search = () => {
         const data = await searchWeather(debouncedSearchInput);
         if (data) {
           setSuggestions(data);
-          setShowDropdown(true);
-        } else {
-          setSuggestions([]);
-          setShowDropdown(false);
         }
       } else {
         setSuggestions([]);
-        setShowDropdown(false);
       }
     };
     fetchSuggestions();
@@ -37,9 +40,10 @@ const Search = () => {
         className={
           "bg-indigo-50 border-none outline-none rounded-md py-2 px-5 w-full focus:outline-indigo-400"
         }
+        value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
-        onFocus={() => setShowDropdown(true)}
-        onClick={() => setShowDropdown((prevState) => !prevState)}
+        onClick={() => setShowDropdown(!showDropdown)}
+        onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
       />
 
       {showDropdown && suggestions.length > 0 && (
@@ -47,13 +51,28 @@ const Search = () => {
           {suggestions.map((suggestion) => (
             <li
               key={suggestion.id}
-              className={"bg-indigo-50 py-2 px-4 body text-gray-900"}
+              className={
+                "bg-indigo-50 py-2 px-4 body text-gray-900 cursor-pointer"
+              }
+              onClick={() => handleSearch(suggestion.name)}
             >
               {suggestion.name}
               {suggestion.region && `, ${suggestion.region}`}
               {suggestion.country && `, ${suggestion.country}`}
             </li>
           ))}
+        </ul>
+      )}
+
+      {showDropdown && suggestions.length === 0 && (
+        <ul className="absolute top-full left-0 right-0 bg-white rounded-md mt-2 max-h-60 overflow-auto z-10">
+          <li
+            className={
+              "bg-indigo-50 py-2 px-4 body text-gray-900 cursor-pointer"
+            }
+          >
+            No results found
+          </li>
         </ul>
       )}
     </div>
--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,6 @@
 type SearchResults = {
-    id: number,
-    name: string,
-    region?: string,
-    country: string
-}
+  id: number;
+  name: string;
+  region?: string;
+  country?: string;
+};
