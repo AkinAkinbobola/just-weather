1e9a6a3eedf08aed97efec35b447b68300a9aef9
Akin Akinbobola <akinbobola71@gmail.com>
2024-05-31 20:37:37 +0100

    Add bds tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,24 @@ import Current from "@/components/Current";
 import { Suspense } from "react";
 import HourlyForecast from "@/components/HourlyForecast";
 import WeatherDetails from "@/components/WeatherDetails";
+import Head from "next/head";
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      bds: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
 const Home = () => {
   return (
     <main className={"grid grid-cols-12"}>
+      <Head>
+        <bds />
+      </Head>
       <div className={"md:col-span-12"}>
         <Suspense>
           <Current />
