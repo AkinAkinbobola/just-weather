4e85455c4f067d6ac53d4b0b9b4132bd679594c3
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-08 10:58:58 +0100

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -12,32 +12,47 @@ const Home = () => {
   const lat = Number(searchParams.get("lat"));
   const lon = Number(searchParams.get("lon"));
   useEffect(() => {
-    if (!isNaN(lat) && !isNaN(lon) && lat !== 0 && lon !== 0) {
-      setLocation({
-        latitude: lat,
-        longitude: lon,
-      });
-    } else {
-      const success = (pos: GeolocationPosition) => {
-        const coords = pos.coords;
-        setLocation(coords);
-      };
+    const handleLocation = () => {
+      if (!isNaN(lat) && !isNaN(lon) && lat !== 0 && lon !== 0) {
+        setLocation({ latitude: lat, longitude: lon });
+      } else {
+        const success = (pos: GeolocationPosition) => {
+          const {
+            latitude,
+            longitude,
+            accuracy,
+            altitude,
+            altitudeAccuracy,
+            heading,
+            speed,
+          } = pos.coords;
+          setLocation({
+            latitude,
+            longitude,
+            accuracy,
+            altitude,
+            altitudeAccuracy,
+            heading,
+            speed,
+          });
+        };
 
-      const error = (err: GeolocationPositionError) => {
-        setLocation({
-          latitude: 37.7749,
-          longitude: -122.4194,
-        });
-      };
+        const error = (err: GeolocationPositionError) => {
+          console.error(err);
+          setLocation({ latitude: 37.7749, longitude: -122.4194 });
+        };
 
-      const options = {
-        maximumAge: 0,
-        enableHighAccuracy: true,
-        timeout: 5000,
-      };
+        const options = {
+          maximumAge: 0,
+          enableHighAccuracy: true,
+          timeout: 5000,
+        };
 
-      navigator.geolocation.getCurrentPosition(success, error, options);
-    }
+        navigator.geolocation.getCurrentPosition(success, error, options);
+      }
+    };
+
+    handleLocation();
   }, [lat, lon]);
   return (
     <main>
